04f090756574750f657f8060dc7653c212c0923b
sharingplay <36578275+sharingplay@users.noreply.github.com>
2018-09-13 00:18:25 -0600

    Add files via upload

---
+++ interfaz/application/control.java
@@ -0,0 +1,83 @@
+package application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class control {
+	@FXML
+	public void mensaje1(ActionEvent evento) {
+		System.out.println("Boton 1");
+	}
+	public void mensaje2(ActionEvent evento1) {
+		System.out.println("Boton 2");
+	}
+	public void mensaje3(ActionEvent evento2) {
+		System.out.println("Boton 3");
+	}
+	public void mensaje4(ActionEvent evento3) {
+		System.out.println("Boton 4");
+	}
+	public void mensaje5(ActionEvent evento4) {
+		System.out.println("Boton 5");
+	}
+	public void mensaje6(ActionEvent evento5) {
+		System.out.println("Boton 6");
+	}
+	public void mensaje7(ActionEvent evento6) {
+		System.out.println("Boton 7");
+	}
+	public void mensaje8(ActionEvent evento7) {
+		System.out.println("Boton 8");
+	}
+	public void mensaje9(ActionEvent evento8) {
+		System.out.println("Boton 9");
+	}
+	public void mensaje10(ActionEvent evento9) {
+		System.out.println("Boton 10");
+	}
+	public void mensaje11(ActionEvent evento10) {
+		System.out.println("Boton 11");
+	}
+	public void mensaje12(ActionEvent evento11) {
+		System.out.println("Boton 12");
+	}
+	public void mensaje13(ActionEvent evento12) {
+		System.out.println("Boton 13");
+	}
+	public void mensaje14(ActionEvent evento13) {
+		System.out.println("Boton 14");
+	}
+	public void mensaje15(ActionEvent evento14) {
+		System.out.println("Boton 15");
+	}
+	public void mensaje16(ActionEvent evento15) {
+		System.out.println("Boton 16");
+	}
+	public void mensaje17(ActionEvent evento16) {
+		System.out.println("Boton 17");
+	}
+	public void mensaje18(ActionEvent evento17) {
+		System.out.println("Boton 18");
+	}
+	public void mensaje19(ActionEvent evento18) {
+		System.out.println("Boton 19");
+	}
+	public void mensaje20(ActionEvent evento19) {
+		System.out.println("Boton 20");
+	}
+	public void mensaje21(ActionEvent evento20) {
+		System.out.println("Boton 21");
+	}
+	public void mensaje22(ActionEvent evento21) {
+		System.out.println("Boton 22");
+	}
+	public void mensaje23(ActionEvent evento22) {
+		System.out.println("Boton 23");
+	}
+	public void mensaje24(ActionEvent evento23) {
+		System.out.println("Boton 24");
+	}
+	public void mensaje25(ActionEvent evento24) {
+		System.out.println("Boton 25");
+	}
+	
+}
